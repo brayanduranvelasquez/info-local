8f61da3bdd3e14fc9ceee6acc6938c2b9b51e710
AndresMalave <amalavecyt@gmail.com>
2022-12-20 14:29:05 -0400

    feat: add locals img
    * Agregadas imagenes individuales a las cards de los locales

--- src/pages/locals/components/BoxLocals.tsx
+++ src/pages/locals/components/BoxLocals.tsx
@@ -1,14 +1,16 @@
 import { Grid, Box, Typography, Button, Card, CardActions, CardContent } from '@mui/material';
-
-import LocalsImg from '../../../shared/assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 interface IProps {
+  img: string;
   title: string;
+  url: string;
 }
 
 export const BoxLocals = (props: IProps) => {
+  const navigate = useNavigate();
   return (
-    <Grid item xs={12} sm={6} md={4}>
+    <Grid item xs={12} sm={6} md={4} display="flex" justifyContent="center" alignItems="center">
       <Card
         elevation={10}
         sx={{
@@ -19,18 +21,23 @@ export const BoxLocals = (props: IProps) => {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-          borderRadius: 5,
+          borderRadius: 1,
           margin: 2,
+          paddingBottom: 2,
         }}
       >
-        <Box component="img" src={LocalsImg} sx={{ width: '100%', height: '150%' }} />
+        <Box component="img" src={props.img} sx={{ width: '100%', height: '150%' }} />
         <CardContent>
           <Typography variant="h4" fontWeight="bold">
             {props.title}
           </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents
+            except Antarctica
+          </Typography>
         </CardContent>
         <CardActions>
-          <Button variant="contained" color="primary">
+          <Button variant="contained" color="primary" onClick={() => navigate(`${props.url}`)}>
             Ir
           </Button>
         </CardActions>
--- src/pages/locals/index.tsx
+++ src/pages/locals/index.tsx
@@ -1,9 +1,19 @@
-import { Box, Container, Grid, Stack, Typography } from '@mui/material';
+import { Box, Container, Grid, Pagination, Stack, Typography } from '@mui/material';
 import Header from '../../shared/components/Header';
 import Footer from '../../shared/components/Footer';
 import InputSearch from '../../shared/components/InputSearch';
-import { BoxPaginate } from '../../shared/components/BoxPaginate';
 import { BoxLocals } from './components/BoxLocals';
+import Breadcrumb from './components/Breadcrumb';
+
+import Local1IMG from './assets/local1.png';
+import Local2IMG from './assets/local2.png';
+import Local3IMG from './assets/local3.png';
+import Local4IMG from './assets/local4.png';
+import Local5IMG from './assets/local5.png';
+import Local6IMG from './assets/local6.png';
+import Local7IMG from './assets/local7.png';
+import Local8IMG from './assets/local8.png';
+import Local9IMG from './assets/local9.png';
 
 export default function Page() {
   return (
@@ -12,9 +22,7 @@ export default function Page() {
 
       <Box sx={{ minHeight: '100%', color: '#000', paddingY: 5 }}>
         <Container>
-          <Typography variant="h6" fontWeight={500}>
-            Actualmente te encuentras en: Inicio / Categorias / Cuadros
-          </Typography>
+          <Breadcrumb />
           <Typography variant="h4" fontWeight={800}>
             Resultados encontrados (10+)
           </Typography>
@@ -24,17 +32,22 @@ export default function Page() {
           </Stack>
 
           <Grid container direction="row">
-            <BoxLocals title={'Decora tu hogar'} />
-            <BoxLocals title={'Decora tu hogar'} />
-            <BoxLocals title={'Decora tu hogar'} />
-            <BoxLocals title={'Decora tu hogar'} />
-            <BoxLocals title={'Decora tu hogar'} />
-            <BoxLocals title={'Decora tu hogar'} />
+            <BoxLocals title={'Casa del cuadro'} img={Local1IMG} url={'/casa-del-cuadro'} />
+            <BoxLocals title={'Me cuadra'} img={Local2IMG} url={'/'} />
+            <BoxLocals title={'ExpoHogar'} img={Local3IMG} url={'/'} />
+            <BoxLocals title={'Hogareño'} img={Local4IMG} url={'/'} />
+            <BoxLocals title={'Traki'} img={Local5IMG} url={'/'} />
+            <BoxLocals title={'MixHouse'} img={Local6IMG} url={'/'} />
+            <BoxLocals title={'El castillo del cuadro'} img={Local7IMG} url={'/'} />
+            <BoxLocals title={'Arte creativo'} img={Local8IMG} url={'/'} />
+            <BoxLocals title={'DecoraTuHogar'} img={Local9IMG} url={'/'} />
           </Grid>
         </Container>
       </Box>
 
-      <BoxPaginate />
+      <Stack justifyContent="center" alignItems="center" my={5}>
+        <Pagination count={10} variant="outlined" shape="rounded" size="large" />
+      </Stack>
 
       <Footer />
     </>
