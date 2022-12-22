6350113b27d6f757576c6c733c83d57e9fdbf0c1
brayanduranvelasquez <brayan13s133@gmail.com>
2022-12-22 10:44:12 -0400

    feat: added parallax effect in SearcherSection

--- src/pages/home/styled-components/StyledStackImg.ts
+++ src/pages/home/styled-components/StyledStackImg.ts
@@ -1,13 +1,18 @@
 import styled from '@emotion/styled';
 import { Stack } from '@mui/material';
+
+// Local
 import backgroundImg from '../assets/background.png';
 
 const StyledStackImg = styled(Stack)({
   maxWidth: '100%',
   height: `calc(100vh - 200px)`,
+  maxHeight: '550px',
   background: `url(${backgroundImg}) no-repeat`,
-  backgroundSize: 'cover',
+  backgroundAttachment: 'fixed',
   backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
 });
 
 export { StyledStackImg };
