e53953f1ad8fd9c8919904ff9c6aeab9cf4bca73
brayanduranvelasquez <brayan13s133@gmail.com>
2022-12-22 10:37:52 -0400

    feat: added loader before load image

--- src/pages/categories/pages/adornos/pages/cuadros/components/CardLocal.tsx
+++ src/pages/categories/pages/adornos/pages/cuadros/components/CardLocal.tsx
@@ -1,5 +1,9 @@
-import { Grid, Box, Typography, Button, Card, CardActions, CardContent } from '@mui/material';
+import { Grid, Box, Typography, CardContent, Stack, Skeleton } from '@mui/material';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
+// Local
+import { StyledCardLocal } from '../styled-components/StyledCardLocal';
 
 interface IProps {
   img: string;
@@ -8,49 +12,30 @@ interface IProps {
 }
 
 export default function CardLocal(props: IProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const navigate = useNavigate();
 
   return (
-    <Grid item xs={12} sm={6} md={4} display="flex" justifyContent="center" alignItems="center">
-      <Card
-        elevation={10}
-        sx={{
-          maxWidth: 345,
-          width: '90%',
-          height: '90%',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 1,
-          margin: 2,
-          paddingBottom: 2,
-          transition: '0.3s all',
-
-          '&:hover': {
-            transform: 'scale(1.05)',
-          },
-        }}
-      >
-        <Box component="img" src={props.img} sx={{ width: '100%', height: '150%' }} />
+    <Grid item xs={12} sm={6} md={4} display="flex" justifyContent="center" alignItems="center" padding={2.5}>
+      <StyledCardLocal elevation={10} onClick={() => props.url && navigate(props.url)}>
+        {isLoading && <Skeleton variant="rectangular" width="100%" height={200} />}
+        {!isLoading && <Box width="100%" height={200} sx={{ background: `url(${props.img})` }} />}
 
         <CardContent>
-          <Typography variant="h4" fontWeight="bold">
-            {props.title}
-          </Typography>
+          <Stack mb={1.5}>
+            <Typography variant="h6" fontWeight="bold">
+              {props.title}
+            </Typography>
+          </Stack>
 
           <Typography variant="body2" color="text.secondary">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate impedit dolore ipsum culpa, laborum, nemo
             eveniet temporibus.
           </Typography>
         </CardContent>
+      </StyledCardLocal>
 
-        <CardActions>
-          <Button variant="contained" color="primary" onClick={() => props.url && navigate(props.url)}>
-            Ir
-          </Button>
-        </CardActions>
-      </Card>
+      <Box component="img" src={props.img} display={'none'} onLoad={() => setIsLoading(false)} />
     </Grid>
   );
 }
